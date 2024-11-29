b9fcc9e9cd13e3405408a5a17d72cd7b8326a64d
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-29 22:35:10 +0900

    各種素材を追加

---
+++ Sample_AR-GunMan_Replace_SwiftUI/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  Sample_AR-GunMan_Replace_SwiftUI
+//
+//  Created by ウルトラ深瀬 on 29/11/24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
