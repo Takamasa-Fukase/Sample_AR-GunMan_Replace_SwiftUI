7bd623b8c6c4100f7e61935ac595fe309e2f1b41
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-29 22:16:23 +0900

    Initial Commit

---
+++ Sample_AR-GunMan_Replace_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sample_AR-GunMan_Replace_SwiftUI
+//
+//  Created by ウルトラ深瀬 on 29/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
