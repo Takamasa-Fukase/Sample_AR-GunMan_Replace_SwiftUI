d1d8bb020bbf1fe6e8988134edf8fe25d28064f5
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-18 00:41:29 +0900

    AR関連ファイルをパッケージに移動

---
+++ ARShootingApp/Sources/ARShootingApp/WeaponObjectData.swift
@@ -0,0 +1,19 @@
+//
+//  WeaponObjectData.swift
+//  Sample_AR-GunMan_Replace
+//
+//  Created by ウルトラ深瀬 on 18/12/24.
+//
+
+import Foundation
+
+struct WeaponObjectData {
+    let weaponId: Int
+    let objectFilePath: String
+    let rootObjectName: String
+    let weaponObjectName: String
+    let targetHitParticleFilePath: String?
+    let targetHitParticleRootObjectName: String?
+    let isGunnerHandShakingAnimationEnabled: Bool
+    let isRecoilAnimationEnabled: Bool
+}
