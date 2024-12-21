45025e8ad65c7dfa48f016f016c518cd29b053c6
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-21 12:26:53 +0900

    複雑なテストデータの一部を日本語に変更

--- WeaponControlMotion/Tests/WeaponControlMotionTests/TestData.swift
+++ WeaponControlMotion/Tests/WeaponControlMotionTests/TestData.swift
@@ -10,73 +10,73 @@ import Foundation
 
 // MARK: 発射モーション判定の加速度条件の境界値データ
 // 条件: 合成値 >= 1.5
-//  - 合成値が 1.501264 になるので満たしている
-let validFireMotionAcceleration = DummyCMAccelerometerData(x: 0, y: 1.0, z: 0.708)
-//  - 合成値が 1.499849 になるので満たしていない
-let invalidFireMotionAcceleration = DummyCMAccelerometerData(x: 0, y: 1.0, z: 0.707)
+//  - 合成値が 1.501264 になるので成功
+let 発射モーション加速度成功データ = DummyCMAccelerometerData(x: 0, y: 1.0, z: 0.708)
+//  - 合成値が 1.499849 になるので失敗
+let 発射モーション加速度失敗データ = DummyCMAccelerometerData(x: 0, y: 1.0, z: 0.707)
 
 
 // MARK: 発射モーション判定のジャイロ条件の境界値データ
 // 条件: 合成値 < 10
-//  - 合成値が 9.998244 になるので満たしている
-let validFireMotionGyro = DummyCMGyroData(x: 0, y: 0, z: 3.162)
-//  - 合成値が 10.004568 になるので満たしていない
-let invalidFireMotionGyro = DummyCMGyroData(x: 0, y: 0, z: 3.163)
+//  - 合成値が 9.998244 になるので成功
+let 発射モーションジャイロ成功データ = DummyCMGyroData(x: 0, y: 0, z: 3.162)
+//  - 合成値が 10.004568 になるので失敗
+let 発射モーションジャイロ失敗データ = DummyCMGyroData(x: 0, y: 0, z: 3.163)
 
 
 // MARK: リロードモーション判定のジャイロ条件の境界値データ
 // 条件: 合成値 >= 10
-//  - 合成値が 10.004568 になるので満たしている
-let validReloadMotionGyro = DummyCMGyroData(x: 0, y: 0, z: 3.163)
-//  - 合成値が 9.998244 になるので満たしていない
-let invalidReloadMotionGyro = DummyCMGyroData(x: 0, y: 0, z: 3.162)
+//  - 合成値が 10.004568 になるので成功
+let リロードモーションジャイロ成功データ = DummyCMGyroData(x: 0, y: 0, z: 3.163)
+//  - 合成値が 9.998244 になるので失敗
+let リロードモーションジャイロ失敗データ = DummyCMGyroData(x: 0, y: 0, z: 3.162)
 
 
 // MARK: テストケース作成・更新時に使用する
 func checkTestDataCompositeValues() {
     print("\n\n=== checkTestDataCompositeValues開始 ===\n")
     
-    let compositeOfValidFireMotionAcceleration = CompositeCalculator.getCompositeValue(
-        x: validFireMotionAcceleration.acceleration.x,
-        y: validFireMotionAcceleration.acceleration.y,
-        z: validFireMotionAcceleration.acceleration.z
+    let compositeOf発射モーション加速度成功データ = CompositeCalculator.getCompositeValue(
+        x: 発射モーション加速度成功データ.acceleration.x,
+        y: 発射モーション加速度成功データ.acceleration.y,
+        z: 発射モーション加速度成功データ.acceleration.z
     )
-    print("compositeOfValidFireMotionAcceleration: \(compositeOfValidFireMotionAcceleration)")
+    print("compositeOf発射モーション加速度成功データ: \(compositeOf発射モーション加速度成功データ)")
     
-    let compositeOfInvalidFireMotionAcceleration = CompositeCalculator.getCompositeValue(
-        x: invalidFireMotionAcceleration.acceleration.x,
-        y: invalidFireMotionAcceleration.acceleration.y,
-        z: invalidFireMotionAcceleration.acceleration.z
+    let compositeOf発射モーション加速度失敗データ = CompositeCalculator.getCompositeValue(
+        x: 発射モーション加速度失敗データ.acceleration.x,
+        y: 発射モーション加速度失敗データ.acceleration.y,
+        z: 発射モーション加速度失敗データ.acceleration.z
     )
-    print("compositeOfInvalidFireMotionAcceleration: \(compositeOfInvalidFireMotionAcceleration)")
+    print("compositeOf発射モーション加速度失敗データ: \(compositeOf発射モーション加速度失敗データ)")
     
-    let compositeOfValidFireMotionGyro = CompositeCalculator.getCompositeValue(
-        x: validFireMotionGyro.rotationRate.x,
-        y: validFireMotionGyro.rotationRate.y,
-        z: validFireMotionGyro.rotationRate.z
+    let compositeOf発射モーションジャイロ成功データ = CompositeCalculator.getCompositeValue(
+        x: 発射モーションジャイロ成功データ.rotationRate.x,
+        y: 発射モーションジャイロ成功データ.rotationRate.y,
+        z: 発射モーションジャイロ成功データ.rotationRate.z
     )
-    print("compositeOfValidFireMotionGyro: \(compositeOfValidFireMotionGyro)")
+    print("compositeOf発射モーションジャイロ成功データ: \(compositeOf発射モーションジャイロ成功データ)")
     
-    let compositeOfInvalidFireMotionGyro = CompositeCalculator.getCompositeValue(
-        x: invalidFireMotionGyro.rotationRate.x,
-        y: invalidFireMotionGyro.rotationRate.y,
-        z: invalidFireMotionGyro.rotationRate.z
+    let compositeOf発射モーションジャイロ失敗データ = CompositeCalculator.getCompositeValue(
+        x: 発射モーションジャイロ失敗データ.rotationRate.x,
+        y: 発射モーションジャイロ失敗データ.rotationRate.y,
+        z: 発射モーションジャイロ失敗データ.rotationRate.z
     )
-    print("compositeOfInvalidFireMotionGyro: \(compositeOfInvalidFireMotionGyro)")
+    print("compositeOf発射モーションジャイロ失敗データ: \(compositeOf発射モーションジャイロ失敗データ)")
     
-    let compositeOfValidReloadMotionGyro = CompositeCalculator.getCompositeValue(
-        x: validReloadMotionGyro.rotationRate.x,
-        y: validReloadMotionGyro.rotationRate.y,
-        z: validReloadMotionGyro.rotationRate.z
+    let compositeOfリロードモーションジャイロ成功データ = CompositeCalculator.getCompositeValue(
+        x: リロードモーションジャイロ成功データ.rotationRate.x,
+        y: リロードモーションジャイロ成功データ.rotationRate.y,
+        z: リロードモーションジャイロ成功データ.rotationRate.z
     )
-    print("compositeOfValidReloadMotionGyro: \(compositeOfValidReloadMotionGyro)")
+    print("compositeOfリロードモーションジャイロ成功データ: \(compositeOfリロードモーションジャイロ成功データ)")
     
-    let compositeOfInvalidReloadMotionGyro = CompositeCalculator.getCompositeValue(
-        x: invalidReloadMotionGyro.rotationRate.x,
-        y: invalidReloadMotionGyro.rotationRate.y,
-        z: invalidReloadMotionGyro.rotationRate.z
+    let compositeOfリロードモーションジャイロ失敗データ = CompositeCalculator.getCompositeValue(
+        x: リロードモーションジャイロ失敗データ.rotationRate.x,
+        y: リロードモーションジャイロ失敗データ.rotationRate.y,
+        z: リロードモーションジャイロ失敗データ.rotationRate.z
     )
-    print("compositeOfInvalidReloadMotionGyro: \(compositeOfInvalidReloadMotionGyro)")
+    print("compositeOfリロードモーションジャイロ失敗データ: \(compositeOfリロードモーションジャイロ失敗データ)")
     
     print("\n=== checkTestDataCompositeValues終了 ===\n\n")
 }
