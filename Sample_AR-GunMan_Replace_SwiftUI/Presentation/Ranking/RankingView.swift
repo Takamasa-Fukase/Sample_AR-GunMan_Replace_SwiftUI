528a6d220fe2b09ec6388461dd28b5686abb6877
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-25 00:09:33 +0900

    レイアウト調整

--- Sample_AR-GunMan_Replace_SwiftUI/Presentation/Ranking/RankingView.swift
+++ Sample_AR-GunMan_Replace_SwiftUI/Presentation/Ranking/RankingView.swift
@@ -8,39 +8,70 @@
 import SwiftUI
 
 struct RankingView: View {
+    var rankingList: [Ranking]
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 ZStack {
-                    ZStack {
-                        Color.goldLeaf
-                            .frame(height: 30)
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                        Text("WORLD RANKING")
-                            .font(.custom("Copperplate Bold", size: 30))
-                        HStack {
-                            Spacer()
-                            
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "xmark")
-                                    .tint(.blackSteel)
-                                    .font(.system(size: 17, weight: .bold))
-                                    .frame(width: 60)
-                            }
+                    // 外側のダークブラウン色の枠線
+                    RoundedRectangle(cornerRadius: 2)
+                        .stroke(Color.customDarkBrown, lineWidth: 5)
+                        .padding(.top, 15)
+                        .padding(.all, 2.4)
+
+                    VStack(spacing: 0) {
+                        // タイトル部分と閉じるボタン
+                        titleView
+                            .padding(.bottom, 8)
+
+                        ZStack {
+                            // ランキング
+                            RankingListView(rankingList: rankingList)
+
+                            // 内側の枠線
+                            RoundedRectangle(cornerRadius: 3)
+                                .stroke(Color.goldLeaf, lineWidth: 7)
+                                .padding(.all, 3.4)
                         }
                     }
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 10))
                 }
-                .frame(width: geometry.size.width * 0.6)
+                .frame(width: (geometry.size.width * 0.6) + 20)
                 .padding(EdgeInsets(top: 24, leading: 0, bottom: 30, trailing: 0))
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.gray)
+        }
+    }
+    
+    private var titleView: some View {
+        ZStack {
+            Color.goldLeaf
+                .frame(height: 30)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            
+            Text("WORLD RANKING")
+                .font(.custom("Copperplate Bold", size: 30))
+            
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .tint(.blackSteel)
+                        .font(.system(size: 17, weight: .bold))
+                        .frame(width: 60)
+                }
+            }
         }
     }
 }
 
 #Preview {
-    RankingView()
+    RankingView(rankingList: Array<Int>(1...100).map({
+        return .init(score: Double(101 - $0), userName: "ユーザー\($0)")
+    }))
+    .background(.black)
 }
