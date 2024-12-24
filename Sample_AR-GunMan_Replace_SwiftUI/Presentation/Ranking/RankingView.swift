e26d1e9d44b8602cc213c64e2dad61d53ade508f
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-24 23:31:21 +0900

    RankingViewの途中

--- Sample_AR-GunMan_Replace_SwiftUI/Presentation/Ranking/RankingView.swift
+++ Sample_AR-GunMan_Replace_SwiftUI/Presentation/Ranking/RankingView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct RankingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                ZStack {
+                    ZStack {
+                        Color.goldLeaf
+                            .frame(height: 30)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                        Text("WORLD RANKING")
+                            .font(.custom("Copperplate Bold", size: 30))
+                        HStack {
+                            Spacer()
+                            
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "xmark")
+                                    .tint(.blackSteel)
+                                    .font(.system(size: 17, weight: .bold))
+                                    .frame(width: 60)
+                            }
+                        }
+                    }
+                }
+                .frame(width: geometry.size.width * 0.6)
+                .padding(EdgeInsets(top: 24, leading: 0, bottom: 30, trailing: 0))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.gray)
+        }
     }
 }
 
